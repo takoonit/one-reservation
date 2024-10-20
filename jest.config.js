a0a7b0bc0738574ffca18eef878b5e055bddbebf
Takoon <takoon.it@gmail.com>
2024-10-21 02:51:47 +0700

    Add coverage and test coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
     },
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 };
   
