25da89ab8f3a9f00f682cc8b122afcc086f9eff2
Takoon <takoon.it@gmail.com>
2024-10-20 20:20:55 +0700

    Add Jest testing script

--- src/test/reservation.test.js
+++ src/test/reservation.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../../app");
 // const reservationService = require('../services/reservationService');
 // jest.mock('../services/reservationService');
 
