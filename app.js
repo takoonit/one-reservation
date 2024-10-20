751fe2e954abcb8a28db6a0501e47c1881523a95
Takoon <takoon.it@gmail.com>
2024-10-20 19:16:23 +0700

    Simplify server startup logging

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const {setupMiddlewares} = require("./src/config/middleware");
-//const {setupKafka} = require("./src/config/kafka");
 const {logger, setupLogger} = require("./src/config/logger");
 const {setupSwagger} = require("./src/config/swagger");
 const {initializeRoutes} = require("./src/routes/initializeRoutes");
@@ -19,16 +18,7 @@ setupSwagger(app);
 // Set up routes
 initializeRoutes(app);
 
-// Set up Kafka event handling
-//setupKafka(app);
-
 // Listening
 app.listen(PORT, (err) => {
-	if (err) {
-		logger.error(`Failed to start server on port ${PORT}`, err);
-	} else {
-		logger.info(`Server is running on port ${PORT}`);
-	}
+	err ? logger.error(`Failed to start server on port ${PORT}`, err) : logger.info(`Server is running on port ${PORT}`);
 });
-// Export app for testing
-module.exports = app;
