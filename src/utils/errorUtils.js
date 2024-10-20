dfb57d924d605bf1a2777c9f20948dfd0fe107ba
Takoon <takoon.it@gmail.com>
2024-10-21 02:54:00 +0700

    Add extra parameter to AppError and extend errorTypes

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -8,10 +8,12 @@ class AppError extends Error {
      * Creates an instance of AppError.
      * @param {string} message - The error message.
      * @param {number} statusCode - HTTP status code associated with the error.
+     * @param {object} [extra={}] - Additional data to include with the error
      */
-    constructor(message, statusCode) {
+    constructor(message, statusCode, extra = {}) {
         super(message);
         this.statusCode = statusCode;
+        this.extra = extra;
         Error.captureStackTrace(this, this.constructor);
     }
 }
@@ -20,10 +22,14 @@ class AppError extends Error {
  * Collection of predefined error messages
  */
 const errorMessages = {
-    TABLES_ALREADY_INITIALIZED: "Tables are already initialized",
-    INVALID_TABLE_COUNT: "Invalid table count",
-    INVALID_MAX_RESERVE_PERCENTAGE: "Invalid max_reserve_percentage",
-    INTERNAL_SERVER_ERROR: "An internal server error occurred",
+    TABLES_ALREADY_INITIALIZED: "Tables are already initialized.",
+    INVALID_TABLE_COUNT: "Invalid table count.",
+    INVALID_MAX_RESERVE_PERCENTAGE: "Invalid max_reserve_percentage.",
+    INTERNAL_SERVER_ERROR: "An internal server error occurred.",
+    TABLES_NOT_INITIALIZED: "Tables are not initialized yet, Please ensure to initialize the number of tables first and try again.",
+    INVALID_CUSTOMER_COUNT: "Invalid customer count.",
+    NOT_ENOUGH_TABLES: "Not enough tables available.",
+    MAX_RESERVATION_EXCEEDED: "Requested seats exceed the allowed limit per one reservation."
 };
 
 /**
@@ -32,8 +38,12 @@ const errorMessages = {
 const errorTypes = {
     TABLES_ALREADY_INITIALIZED: new AppError(errorMessages.TABLES_ALREADY_INITIALIZED, 409),
     INVALID_TABLE_COUNT: new AppError(errorMessages.INVALID_TABLE_COUNT, 400),
-    INVALID_MAX_RESERVE_PERCENTAGE: new AppError(errorMessages.INVALID_MAX_RESERVE_PERCENTAGE, 400),
+    INVALID_MAX_ALLOWED_PERCENTAGE: new AppError(errorMessages.INVALID_MAX_RESERVE_PERCENTAGE, 400),
     INTERNAL_SERVER_ERROR: new AppError(errorMessages.INTERNAL_SERVER_ERROR, 500),
+    TABLES_NOT_INITIALIZED: new AppError(errorMessages.TABLES_NOT_INITIALIZED, 409),
+    INVALID_CUSTOMER_COUNT: new AppError(errorMessages.INVALID_CUSTOMER_COUNT, 400),
+    NOT_ENOUGH_TABLES: new AppError(errorMessages.NOT_ENOUGH_TABLES, 400),
+    MAX_RESERVATION_EXCEEDED: (extra) => new AppError(errorMessages.MAX_RESERVATION_EXCEEDED, 400, this.extra),
 };
 
 module.exports = {
